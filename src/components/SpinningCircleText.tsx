fdcd1da119cfa1c56ac10eafab01a29c120f2bb9
Valentina Marino <valenmarinocol@gmail.com>
2026-03-09 09:25:19 -0500

    added fade in animation to spinning circle

--- src/components/SpinningCircleText.tsx
+++ src/components/SpinningCircleText.tsx
@@ -23,46 +23,58 @@ export default function SpinningCircleText({
 
   return (
     <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 1.2,
+        ease: [0.22, 1, 0.36, 1], // elegant easeOutExpo feel
+      }}
       className="relative flex items-center justify-center"
-      animate={{ rotate: 360 }}
-      transition={{ duration, ease: "linear", repeat: Infinity }}
       style={{ width: size, height: size }}
     >
-      {/* CENTER SVG */}
-      <div className="absolute w-[40%] h-[40%]">
-        <Image
-          src="/icons/carmela-figure.svg"
-          alt="Carmela figure"
-          fill
-          className="object-contain"
-        />
-      </div>
-
-      {/* TEXT CIRCLE */}
-      <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`}>
-        <defs>
-          <path
-            id={pathId}
-            d={`
-              M ${size / 2}, ${size / 2}
-              m -${radius}, 0
-              a ${radius},${radius} 0 1,1 ${radius * 2},0
-              a ${radius},${radius} 0 1,1 -${radius * 2},0
-            `}
+      {/* ROTATION LAYER */}
+      <motion.div
+        className="relative flex items-center justify-center"
+        animate={{ rotate: 360 }}
+        transition={{ duration, ease: "linear", repeat: Infinity }}
+        style={{ width: size, height: size }}
+      >
+        {/* CENTER SVG */}
+        <div className="absolute w-[40%] h-[40%]">
+          <Image
+            src="/icons/carmela-figure.svg"
+            alt="Carmela figure"
+            fill
+            className="object-contain"
           />
-        </defs>
+        </div>
 
-        <text
-          fill="white"
-          fontSize={fontSize}
-          letterSpacing="2px"
-          className={fontClass}
-        >
-          <textPath href={`#${pathId}`} startOffset="0%">
-            {text}
-          </textPath>
-        </text>
-      </svg>
+        {/* TEXT CIRCLE */}
+        <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`}>
+          <defs>
+            <path
+              id={pathId}
+              d={`
+                M ${size / 2}, ${size / 2}
+                m -${radius}, 0
+                a ${radius},${radius} 0 1,1 ${radius * 2},0
+                a ${radius},${radius} 0 1,1 -${radius * 2},0
+              `}
+            />
+          </defs>
+
+          <text
+            fill="white"
+            fontSize={fontSize}
+            letterSpacing="2px"
+            className={fontClass}
+          >
+            <textPath href={`#${pathId}`} startOffset="0%">
+              {text}
+            </textPath>
+          </text>
+        </svg>
+      </motion.div>
     </motion.div>
   );
 }
